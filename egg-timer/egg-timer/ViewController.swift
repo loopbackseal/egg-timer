cd63fc85be59c6d4326983e5ecd129ad895f9443
loopbackseal <young961027@gmail.com>
2021-12-24 18:06:08 +0900

    make timer without kill

--- egg-timer/egg-timer/ViewController.swift
+++ egg-timer/egg-timer/ViewController.swift
@@ -9,16 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let hardnessTime = ["soft": 5, "medium": 7, "hard": 12]
-    
+    let eggTime = ["soft": 5, "medium": 7, "hard": 12]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    var counter: Int = 0
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds")
+            counter -= 1
+        }
+    }
+    
     @IBAction func eggPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle!
-        print(hardnessTime[hardness]!)
+        counter = eggTime[hardness]! * 60
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
 }
